32943849041bad6702553116c2ebf8c3ad9e2a99
SubrataDevMohanta <shuvomohanta1234@gmail.com>
2023-03-10 17:35:53 +0600

    some

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me.png'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
 
 const About = () => {
     return (
@@ -9,19 +10,30 @@ const About = () => {
             <h2>About Me</h2>
 
             <div className="container about__container">
-            <div className="about__me">
-                <div className="about__me-image">
-                    <img src="{ME}" alt="About Image" />
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src="ME" alt="About Image" />
+                    </div>
                 </div>
-            </div>
-            <div className="about__content">
-                <div className="about__cards">
-                    <article className='about__card'>
-                        <h5>Experience</h5>
-                        <small>2+ years Working</small>
-                    </article>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__' />
+                            <h5>Experience</h5>
+                            <small>2+ years Working</small>
+                        </article>
+                        <article className='about__card'>
+                            <FaAward className='about__' />
+                            <h5>Experience</h5>
+                            <small>2+ years Working</small>
+                        </article>
+                        <article className='about__card'>
+                            <FaAward className='about__' />
+                            <h5>Experience</h5>
+                            <small>2+ years Working</small>
+                        </article>
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
     )
